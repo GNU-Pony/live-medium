27a84532f297189b398bca27a277db178488f696
Mattias Andrée <maandree@operamail.com>
2013-03-23 18:31:48 +0100

    add file that was missed due to gitignore miss
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ auxiliary-config.mk
@@ -0,0 +1,6 @@
+GNU_PONY_INITRAM = ../initram
+CHOST = $(ARCH)-unknown-linux-gnu
+KERNEL_MIRROR = https://ftp.kernel.org/pub/linux
+MBR = /usr/lib/syslinux/mbr.bin
+INITCPIO_COMPRESS = xz -e9
+CLEAN_DIR =
