b51a14cee005998dd358246c1714a27a4d1036b4
Mattias Andrée <maandree@operamail.com>
2013-04-25 11:55:44 +0200

    m
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- mkfiles/packages.mk
+++ mkfiles/packages.mk
@@ -2,10 +2,10 @@ PACKAGES = $(shell find pkgs/ | grep '\.scroll$$' | grep -v '\x23' | sed -e 's:^
 
 CLEAN_DIR += $(foreach $(PACKAGES), PACKAGE, pkgs/$(PACKAGE))
 
-packages: $(foreach $(PACKAGES), PACKAGE, $(PACKAGE).pkgs.tar.$(PKG_COMPRESS_EXT))
+packages: $(foreach $(PACKAGES), PACKAGE, $(PACKAGE).pkg.tar.$(PKG_COMPRESS_EXT))
 
 %s.pkg.tar.$(PKG_COMPRESS_EXT): pkgs/%s.scroll
 	mkdir -p "pkgs/$*/start" "pkgs/$*/install"
-	ARCH=$(ARCH) HOST=$(HOST) "$(SPIKE)/spikeless" "$<" "pkgs/$*/start" "pkgs/$*/install"
+	ARCH=$(ARCH) HOST=$(HOST) MAKEFLAGS= "$(SPIKE)/spikeless" "$<" "pkgs/$*/start" "pkgs/$*/install"
 	cd "pkgs/$*/install" && tar --create * | $(PKG_COMPRESS) > "$*.pkg.tar.$(PKG_COMPRESS_EXT)"
 
