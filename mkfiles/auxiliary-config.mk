87ce0888cd426f338773fcb9afdf8556a04e0a66
Mattias Andrée <maandree@operamail.com>
2013-04-25 10:44:17 +0200

    m
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- mkfiles/auxiliary-config.mk
+++ mkfiles/auxiliary-config.mk
@@ -1,5 +1,5 @@
 GNU_PONY_INITRAM = ../initram
-CHOST = $(ARCH)-unknown-linux-gnu
+HOST = $(ARCH)-unknown-linux-gnu
 KERNEL_MIRROR = https://ftp.kernel.org/pub/linux
 SYSLINUX_DIR = /usr/lib/syslinux
 MBR = $(SYSLINUX_DIR)/mbr.bin
