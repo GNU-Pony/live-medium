d892d87d2d831bbc5b830199c302cad03efb003c
Mattias Andrée <maandree@operamail.com>
2013-04-25 12:06:01 +0200

    m
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- mkfiles/packages.mk
+++ mkfiles/packages.mk
@@ -1,11 +1,11 @@
 PACKAGES = $(shell find pkgs/ | grep '\.scroll$$' | grep -v '\x23' | sed -e 's:^pkgs/::g' -e 's:\.scroll$$::g')
 
-CLEAN_DIR += $(foreach $(PACKAGES), PACKAGE, pkgs/$(PACKAGE))
+CLEAN_DIR += $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE))
 
-packages: $(foreach $(PACKAGES), PACKAGE, $(PACKAGE).pkg.tar.$(PKG_COMPRESS_EXT))
+packages: $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE).pkg.tar.$(PKG_COMPRESS_EXT))
 
-%.pkg.tar.$(PKG_COMPRESS_EXT): pkgs/%.scroll
+pkgs/%.pkg.tar.$(PKG_COMPRESS_EXT): pkgs/%.scroll
 	mkdir -p "pkgs/$*/start" "pkgs/$*/install"
-	ARCH=$(ARCH) HOST=$(HOST) MAKEFLAGS= "$(SPIKE)/spikeless" "$<" "pkgs/$*/start" "pkgs/$*/install"
-	cd "pkgs/$*/install" && tar --create * | $(PKG_COMPRESS) > "$*.pkg.tar.$(PKG_COMPRESS_EXT)"
+	ARCH=$(ARCH) HOST=$(HOST) MAKEFLAGS= "$(SPIKE)/spikeless" "pkgs/$*/scroll" "pkgs/$*/start" "pkgs/$*/install"
+	cd "pkgs/$*/install" && tar --create * | $(PKG_COMPRESS) > "../../$*.pkg.tar.$(PKG_COMPRESS_EXT)"
 
