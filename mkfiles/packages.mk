1871f004ae903c8860bbdd6fc711c3581f507e12
Mattias Andrée <maandree@operamail.com>
2013-04-26 12:28:01 +0200

    misc
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- mkfiles/packages.mk
+++ mkfiles/packages.mk
@@ -2,10 +2,18 @@ PACKAGES = $(shell find pkgs/ | grep '\.scroll$$' | grep -v '\x23' | sed -e 's:^
 
 CLEAN_DIR += $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE))
 
-packages: $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE).pkg.tar.$(PKG_COMPRESS_EXT))
+packages: clean-finalise packages-build
+
+packages-build: $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE).pkg.tar.$(PKG_COMPRESS_EXT))
 
 pkgs/%.pkg.tar.$(PKG_COMPRESS_EXT): pkgs/%.scroll
 	mkdir -p "pkgs/$*/start" "pkgs/$*/install"
-	ARCH=$(ARCH) HOST=$(HOST) MAKEFLAGS= "$(SPIKE)/spikeless" "pkgs/$*.scroll" "pkgs/$*/start" "pkgs/$*/install"
+	PATH="$$(realpath ./tools):$${PATH}" ARCH=$(ARCH) HOST=$(HOST) MAKEFLAGS= \
+	    "$(SPIKE)/spikeless" "pkgs/$*.scroll" "pkgs/$*/start" "pkgs/$*/install"
 	cd "pkgs/$*/install" && tar --create * | $(PKG_COMPRESS) > "../../$*.pkg.tar.$(PKG_COMPRESS_EXT)"
+
+clean-finalise:
+	rm finalise
+	chmod a+x finalise
+	echo '#!/bin/sh' > finalise
 
