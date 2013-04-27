4f772ba23bd1106dca2597289e2218ce80c49bdb
Mattias Andrée <maandree@operamail.com>
2013-04-27 04:43:21 +0200

    scroll fixes
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- mkfiles/packages.mk
+++ mkfiles/packages.mk
@@ -1,19 +1,6 @@
-PKGS_STAGE_1 = acl attr audit bash bzip2 coreutils cracklib cryptsetup curl db dbus device-mapper \
-               dhcpcd dnssec-anchors e2fsprogs expat file findutils gawk gdbm gettext glib2 glibc \
-               gmp grep gzip hwids iana-etc inetutils initscripts-fork iproute2 iputils kbd keyutils \
-               kmod krb5 ldns less libcap libedit libffi libgcrypt libgpg-error libgssglue libldap \
-               libnl libpcap libssh2 libtirpc libusbx nano ncurses netcfg openntpd openssh openssl \
-               pam pam_unix pcre perl popt readline sed shadow sysfsutils systemd sysvinit tar \
-               texinfo tzdata util-linux which xz zlib
+PACKAGES = $(shell find pkgs/ | grep '\.scroll$$' | grep -v '\x23' | sed -e 's:^pkgs/::g' -e 's:\.scroll$$::g' | sort)
 
-PKGS_STAGE_2 = gcc-libs libsasl
-
-
-PKGS = $(PKGS_STAGE_1) $(PKGS_STAGE_2)
-CLEAN_DIR += $(foreach PKG, $(PKGS), pkgs/$(PKG))
-
-
-packages: clean-finalise packages-stage1 packages-stage2
+CLEAN_DIR += $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE))
 
 
 clean-finalise:
@@ -28,29 +15,16 @@ finalise-packages:
 	([ "$(DEVICE)" = "" ] || sudo umount "$(MNT)")
 
 
-pkgs/%.pkg.tar.$(PKG_COMPRESS_EXT): pkgs/%.scroll
+pkgs/%.pkg.tar.$(PKG_COMPRESS_EXT): pkgs/%.scroll clean-finalise
 	mkdir -p "pkgs/$*/start" "pkgs/$*/install"
 	PATH="$$(realpath ./tools):$${PATH}" ARCH=$(ARCH) HOST=$(HOST) MAKEFLAGS= \
 	    "$(SPIKE)/spikeless" "pkgs/$*.scroll" "pkgs/$*/start" "pkgs/$*/install"
 	cd "pkgs/$*/install" && tar --create * | $(PKG_COMPRESS) > "../../$*.pkg.tar.$(PKG_COMPRESS_EXT)"
 
 
-packages-build-stage1: $(foreach PKG, $(PKGS_STAGE_1), pkgs/$(PKG).pkg.tar.$(PKG_COMPRESS_EXT))
-packages-build-stage2: $(foreach PKG, $(PKGS_STAGE_2), pkgs/$(PKG).pkg.tar.$(PKG_COMPRESS_EXT))
-
-
-packages-stage1:
-	make -j$(CPUS) packages-build-stage1
+packages: $(foreach PACKAGE, $(PACKAGES), pkgs/$(PACKAGE).pkg.tar.$(PKG_COMPRESS_EXT))
 	([ "$(DEVICE)" = "" ] || sudo mount "/dev/$(DEVICE)1" "$(MNT)")
 	root=$$(pwd) ; cd "$(MNT)" ; for pkg in $(foreach PKG, $(PKGS_STAGE_1), pkgs/$(PKG).pkg.tar.$(PKG_COMPRESS_EXT)); do \
-	echo "extracting $$pkg" ; tar --get --xz < $$root/$$pkg ; done
-	([ "$(DEVICE)" = "" ] || sudo umount "$(MNT)")
-
-
-packages-stage2:
-	LD_LIBRARY_PATH="$(MNT)" make -j$(CPUS) packages-build-stage2
-	([ "$(DEVICE)" = "" ] || sudo mount "/dev/$(DEVICE)1" "$(MNT)")
-	root=$$(pwd) ; cd "$(MNT)" ; for pkg in $(foreach PKG, $(PKGS_STAGE_2), pkgs/$(PKG).pkg.tar.$(PKG_COMPRESS_EXT)); do \
 	echo "extracting $$pkg" ; tar --get --xz < $$root/$$pkg ; done
 	([ "$(DEVICE)" = "" ] || sudo umount "$(MNT)")
 
